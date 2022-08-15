8754947b96492f1a5a5851a7d799ac142886877f
Sharma Ketan <sharmaketann@gmail.com>
2022-08-15 05:54:27 +0530

    last scroll view fixed

--- src/components/DetailPanel/index.jsx
+++ src/components/DetailPanel/index.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import {BG, CloseWrapper, Em, P, Panel} from './styles'
 import Book from '../Book'
 import {Close} from '../../styles'
 
 const DetailPanel = ({book, closePanel, state}) => {
+  const panelEl = useRef(null)
+  const prevBook = useRef(null)
+
+  useEffect(() => {
+    if (prevBook.current !== book) {
+      panelEl.current.scrollTop = 0
+    }
+    prevBook.current = book
+  }, [book, prevBook])
+
   console.log(state)
   return (
     <>
       <BG onClick={closePanel} $state={state} />
-      <Panel $state={state}>
+      <Panel $state={state} ref={panelEl}>
         <CloseWrapper onClick={closePanel} $state={state}>
           <Close />
         </CloseWrapper>
