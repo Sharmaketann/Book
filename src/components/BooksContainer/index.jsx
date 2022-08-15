d914e3e3cf558679f5ea2d2eac327893d2eb2358
Sharma Ketan <sharmaketann@gmail.com>
2022-08-15 06:10:49 +0530

    scroll behaviour fixed

--- src/components/BooksContainer/index.jsx
+++ src/components/BooksContainer/index.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import {Container, H2, BookList} from './styles'
+import {debounce} from 'lodash-es'
 import Book from '../Book'
 
-const BooksContainer = ({books, pickBook, isPanelOpen}) => (
-  <Container $isPanelOpen={isPanelOpen}>
-    <H2>All Books</H2>
-    <BookList>
-      {books.map((book) => (
-        <Book key={book.id} book={book} pickBook={pickBook} />
-      ))}
-    </BookList>
-  </Container>
-)
+const BooksContainer = ({books, pickBook, isPanelOpen}) => {
+  const [scroll, setScroll] = useState(0)
+  const prevPanelState = useRef(false)
+
+  // function runs when isPanel value changes.
+  useEffect(() => {
+    const handScroll = debounce(() => {
+      setScroll(window.scrollY)
+    }, 100)
+    if (!isPanelOpen) {
+      window.addEventListener('scroll', handScroll)
+    }
+
+    return () => {
+      window.removeEventListener('scroll', handScroll)
+    }
+  }, [isPanelOpen])
+
+  // triggers based on three different variables inside the dependency array
+  useEffect(() => {
+    if (prevPanelState.current && !isPanelOpen) {
+      window.scroll(0, scroll)
+    }
+
+    prevPanelState.current = isPanelOpen
+  }, [isPanelOpen, prevPanelState, scroll])
+
+  console.log(scroll)
+
+  return (
+    <Container $isPanelOpen={isPanelOpen} $top={scroll}>
+      <H2>All Books</H2>
+      <BookList>
+        {books.map((book) => (
+          <Book key={book.id} book={book} pickBook={pickBook} />
+        ))}
+      </BookList>
+    </Container>
+  )
+}
 
 export default BooksContainer
